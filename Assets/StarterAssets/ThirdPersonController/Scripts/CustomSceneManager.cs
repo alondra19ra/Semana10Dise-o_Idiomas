033427001f2b5cf5fdfffec4100ec377dd2d9cf7
alondra19ra <alondra.rojas@tecsup.edu.pe>
2025-10-22 10:54:30 -0500

    salir

--- Assets/StarterAssets/ThirdPersonController/Scripts/CustomSceneManager.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/CustomSceneManager.cs
@@ -1,14 +1,30 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CustomSceneManager : MonoBehaviour
 {
-    void Start()
+    public void LoadScene(string sceneName)
     {
-        
+        SceneManager.LoadScene(sceneName);
     }
 
-    void Update()
+    public void LoadScene(int sceneId)
     {
-        
+        SceneManager.LoadScene(sceneId);
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    private void OnApplicationQuit()
+    {
+        print("Adios....");
     }
 }
